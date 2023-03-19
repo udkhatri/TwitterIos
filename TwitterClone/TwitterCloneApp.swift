67ba8c052ab386834e96849b086b0ecccd93fd80
Uday khatri <udayskhatri@gmail.com>
2023-03-19 14:26:10 -0400

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Uday Khatri on 2023-03-19.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
